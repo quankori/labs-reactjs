81e4ffab900feb1acdb99283c6ea02997a713822
Quan Nguyen <quan.nguyen@tem21039.local>
2023-10-20 12:12:21 +0700

    Calender

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -11,15 +11,28 @@ import {
 } from "../../features/token/tokenAction";
 import { ReadMoreText } from "../../components/ReadMoreText/ReadMoreText";
 import { ChartType } from "../../types/enum";
+import { formatDate } from "../../utils/format";
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { isAfter, addMonths, isBefore } from "date-fns";
+import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
+import DateFnsUtils from "@date-io/date-fns";
+
 export const Detail: React.FC = () => {
   const [currentChart, setCurrentChart] = useState<ChartType>(
     ChartType.OHLC_CHART
   );
+
+  const today = new Date();
+  const lastMonth = new Date(today);
+  lastMonth.setMonth(today.getMonth() - 1);
+
   const [daysOHLCChart, setDaysOHLCChart] = useState<number>(365);
   const { id } = useParams<{ id: string }>();
   const dispatch = useDispatch<AppDispatch>();
   const coin = useSelector((state: RootState) => state.tokens.coin);
   const ohlc = useSelector((state: RootState) => state.tokens.ohlc);
+  const [fromDate, setFromDate] = useState(formatDate(lastMonth.getTime()));
+  const [toDate, setToDate] = useState(formatDate(today.getTime()));
 
   useEffect(() => {
     dispatch(fetechDetailCoin({ tokenId: id }));
@@ -28,6 +41,32 @@ export const Detail: React.FC = () => {
   useEffect(() => {
     dispatch(fetchOHLCCoin({ tokenId: id, days: daysOHLCChart }));
   }, [daysOHLCChart]);
+
+  // useEffect(() => {
+  //   if (!validateDates()) {
+  //     console.log("ss");
+  //     return;
+  //   }
+  //   console.log("ok");
+  // }, [fromDate, toDate]);
+
+  const handleFromShouldDisableDate = (date: MaterialUiPickersDate) => {
+    if (!date) return false;
+    if (toDate) {
+      const [year, month, day] = toDate.split("-").map(Number);
+      return isAfter(date, new Date(year, month - 1, day));
+    }
+    return false;
+  };
+
+  const handleToShouldDisableDate = (date: MaterialUiPickersDate) => {
+    if (!date) return false;
+    if (fromDate) {
+      const [year, month, day] = fromDate.split("-").map(Number);
+      return isBefore(date, new Date(year, month - 1, day));
+    }
+    return false;
+  };
 
   const renderChart = () => {
     switch (currentChart) {
@@ -70,7 +109,47 @@ export const Detail: React.FC = () => {
           </Grid>
         );
       case ChartType.PRICE_CHART:
-        return <p>Hello</p>;
+        return (
+          <Grid container spacing={2} alignItems="center">
+            <Grid>
+              <OHLCChart width={600} height={400} data={ohlc} />
+            </Grid>
+            <Grid item xs={6}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <DatePicker
+                  margin="normal"
+                  id="from-date-picker"
+                  label="From"
+                  format="yyyy/MM/dd"
+                  value={fromDate}
+                  minDate={lastMonth}
+                  maxDate={today}
+                  shouldDisableDate={handleFromShouldDisableDate}
+                  onChange={(newValue) => {
+                    if (newValue) setFromDate(formatDate(newValue.getTime()));
+                  }}
+                />
+              </MuiPickersUtilsProvider>
+            </Grid>
+            <Grid item xs={6}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <DatePicker
+                  margin="normal"
+                  id="to-date-picker"
+                  label="To"
+                  format="yyyy/MM/dd"
+                  value={toDate}
+                  minDate={lastMonth}
+                  maxDate={today}
+                  shouldDisableDate={handleToShouldDisableDate}
+                  onChange={(newValue) => {
+                    if (newValue) setToDate(formatDate(newValue.getTime()));
+                  }}
+                />
+              </MuiPickersUtilsProvider>
+            </Grid>
+          </Grid>
+        );
       default:
         break;
     }
